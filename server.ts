b236593b32f3dd483381f03d8bac7f6e475c45fa
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-06-16 20:04:13 +0300

    add

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import Routes from './Router/index';
+import Router from './Router/index';
 
 const app = express();
 
-app.use(Routes);
+app.use(Router);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
